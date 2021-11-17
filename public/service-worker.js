aef2e87215cd94f716e92b96478f20809d8d92fd
GMMarshall <gmmarshall27@gmail.com>
2021-11-17 15:52:41 -0500

    finsihed fetch function and service-worker.js file

--- public/service-worker.js
+++ public/service-worker.js
@@ -40,4 +40,16 @@ self.addEventListener("fetch", function(event){
         );
         return;
     }
-})
+
+    event.respondWith(
+        fetch(event.request).catch(function(){
+            return caches.match(event.request).then(function(response){
+                if(response) {
+                    return response;
+                } else if (event.request.headers.get("accept").includes("text/html")) {
+                    return caches.match("/");
+                }
+            });
+        })
+    );
+});
