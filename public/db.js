37932aeef3883278c890864f460cb566c9b68494
GMMarshall <gmmarshall27@gmail.com>
2021-11-17 14:45:01 -0500

    finished request on db and save record function

--- public/db.js
+++ public/db.js
@@ -7,3 +7,27 @@ window.shimIndexedDB;
 
 let db;
 const request = indexedDB.open("budget", 1); 
+
+request.onupgradeneeded = ({ target }) => {
+    let db = target.result;
+    db.createObjectStore('pending', { autoIncrement: true });
+};
+
+request.onsuccess = ({ target }) => {
+    db = target.result;
+
+    if (navigator.onLine) {
+        checkDb();
+    }
+};
+
+request.onerror = (e) =>{
+    console.log('Error')
+};
+
+function saveRecord(record) {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+
+    store.add(record);
+}
