59b68d6e05d2d3e49a3c6910cc90b37191b61192
GMMarshall <gmmarshall27@gmail.com>
2021-11-17 15:19:13 -0500

    added files to cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,2 +1,13 @@
 var CACHE_NAME = "my-site-cache-v1";
 const DATA_CACHE_NAME = "data-cache-v1";
+
+var filesToCache = [
+    "/",
+    "/db.js",
+  "/index.js",
+  "/manifest.json",
+  "/styles.css",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
+  "https://cdn.jsdelivr.net/npm/chart.js@2.8.0"
+]
